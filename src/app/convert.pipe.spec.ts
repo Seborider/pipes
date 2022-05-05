45158a16661fbc184b9052ec33e02cc36d3923c6
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-05-05 16:30:26 +0200

    Revert "Html"

--- src/app/convert.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConvertPipe } from './convert.pipe';
-
-describe('ConvertPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ConvertPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
